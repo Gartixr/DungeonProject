55b657b40cb58d76de5a7fb299ddcfc64ad0ee06
KrullVT <krullvanthack@gmail.com>
2020-01-09 20:25:49 +0100

    JUnit

--- src/ProvesJUnit.java
+++ src/ProvesJUnit.java
@@ -6,27 +6,37 @@ class ProvesJUnit {
 
 	@Test
 	void test() {
+//		Instanciem un Hero
 		Hero hero = new Hero("Hero");
-		System.out.println("Hero nom : "+ hero.getName());
-		System.out.println("Hero hp : "+ hero.getHp());
+		assertTrue(hero.getAgility() > 2 & hero.getAgility() < 11);
+		assertTrue(hero.getHp() > 7 & hero.getHp() < 21);
+		assertTrue(hero.getInfluence() > 2 & hero.getInfluence() < 14);
+		assertTrue(hero.getLore() > 2 & hero.getLore() < 14);
+		assertTrue(hero.getObservation() > 2 & hero.getObservation() < 14);
+		assertEquals("Hero", hero.getName());
 	}
-	
+
 	@Test
 	void test1() {
 		Hero hero1 = new Hero("Hero1");
-		System.out.println("Hero1 loot : "+ hero1.loot().getName());
+		Mob mob1 = new Mob("Mob1", 5);
+		hero1.attack(mob1);
+		assertTrue(hero1.getHp() > 0);
+		mob1.attack(hero1);
+		hero1.die(hero1);
+		mob1.die(mob1);
 	}
 
 	@Test
 	void test2() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void test3() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void test4() {
 		fail("Not yet implemented");
