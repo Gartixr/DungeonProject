c7c1c0146c7b58b9e1301be9ca42e7ffca72d65d
Adrian Dumitrica <adidumitrica@gmail.com>
2020-01-13 17:11:12 +0100

    Update ProvesJUnit.java

---
+++ src/ProvesJUnit.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+class ProvesJUnit {
+
+	@Test
+	void testQueComprovaQueElsAtributsDelHeroNoSiguinMesGransOMesPetitsQueElsMinimsIMaxims() {
+//		Instanciem un Hero
+		Hero hero = new Hero("Hero");
+		assertTrue(hero.getAgility() > 2 & hero.getAgility() < 11);
+		assertTrue(hero.getHp() > 7 & hero.getHp() < 21);
+		assertTrue(hero.getInfluence() > 2 & hero.getInfluence() < 14);
+		assertTrue(hero.getLore() > 2 & hero.getLore() < 14);
+		assertTrue(hero.getObservation() > 2 & hero.getObservation() < 14);
+		assertEquals("Hero", hero.getName());
+	}
+
+	@Test
+	void testQueComprovaQueElHeroiAtacaIElMonstreEsProtegeix() {
+		Hero hero1 = new Hero("Hero1");
+		Mob mob1 = new Mob("Mob1", 5);
+		int vidaMonstre = mob1.getHp();
+		hero1.attack(mob1);
+		assertTrue(mob1.getHp() <= vidaMonstre);
+	}
+
+	@Test
+	void test2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void test3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void test4() {
+		fail("Not yet implemented");
+	}
+}
